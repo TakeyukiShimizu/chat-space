a56e9905babaf774f84b5139cc6d80869acf4670
TakeyukiShimizu <take54883@gmail.com>
2020-05-01 12:11:04 +0900

    細かな修正（自動更新機能以外の修正）

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,8 +1,9 @@
 $(function(){
+
   function buildHTML(message){
     if ( message.image ) {
-      let html =
-       `<div class="message-list">
+      var html =
+       `<div class="message-list" data-message-id=${message.id}>
           <div class="message-list_info">
             <div class="message-list__name">
               ${message.user_name}
@@ -20,8 +21,8 @@ $(function(){
         </div>`
       return html;
     } else {
-      let html =
-       `<div class="message-list">
+      var html =
+       `<div class="message-list" data-message-id=${message.id}>
           <div class="message-list_info">
             <div class="message-list__name">
               ${message.user_name}
@@ -39,10 +40,11 @@ $(function(){
       return html;
     };
   }
+
   $('#new_message').on('submit', function(e){
     e.preventDefault();
-    let formData = new FormData(this);
-    let url = $(this).attr('action');
+    var formData = new FormData(this);
+    var url = $(this).attr('action');
     $.ajax({
       url: url,
       type: "POST",
@@ -52,7 +54,7 @@ $(function(){
       contentType: false
     })
     .done(function(data){
-      let html = buildHTML(data);
+      var html = buildHTML(data);
       $('.messages-list').append(html);
       $('form')[0].reset();
       $('.messages-list').animate({ scrollTop: $('.messages-list')[0].scrollHeight});
@@ -62,4 +64,31 @@ $(function(){
       alert("メッセージ送信に失敗しました");
     });
   });
+
+  var reloadMessages = function() {
+    var last_message_id = $('.message-list:last').data("message-id");
+    $.ajax({
+      url: "api/messages",
+      type: 'get',
+      dataType: 'json',
+      data: {id: last_message_id}
+    })
+    .done(function(messages) {
+      if (messages.length !== 0) {
+        var insertHTML = '';
+        $.each(messages, function(i, message) {
+          insertHTML += buildHTML(message)
+        });
+        $('.message-list').append(insertHTML);
+        $('.message-list').animate({ scrollTop: $('.messages-list')[0].scrollHeight});
+      }
+    })
+    .fail(function() {
+      alert('error');
+    });
+  };
+
+  if (document.location.href.match(/\/groups\/\d+\/messages/)) {
+    setInterval(reloadMessages, 7000);
+  }
 });
